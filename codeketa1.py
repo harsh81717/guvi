b8a9b1e4712055bffdc6c6da62c0b02405c6ea23
harsh81717 <45117557+harsh81717@users.noreply.github.com>
2018-11-17 13:27:11 +0530

    Update codeketa1.py

--- codeketa1.py
+++ codeketa1.py
@@ -2,6 +2,6 @@ a=int(input("Enter a number"))
 if a==0:
 	print("number is zero")
 elif a>0:
-  print("number is positive")
+ 	print("number is positive")
 else:
-  print("number is negative")
+ 	print("number is negative")
