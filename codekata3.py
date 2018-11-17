3014c816b78fa620eb6364922aea2666413303a7
harsh81717 <45117557+harsh81717@users.noreply.github.com>
2018-11-17 14:47:22 +0530

    Create codekata3.py

---
+++ codekata3.py
@@ -0,0 +1,8 @@
+ch= input("Enter a character: ")
+if ch in ['a','e','i','o','u','A','E','I','O','U']:
+  print("Vowel")
+elif ch.isalpha()==True:
+  print("Consonent")
+else:
+  print("invalid")
+    
