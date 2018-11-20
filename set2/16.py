1f1d99b007216cf34be714af08399372f3bb2eb4
harsh81717 <45117557+harsh81717@users.noreply.github.com>
2018-11-20 23:58:06 +0530

    Update 16.py

--- set2/16.py
+++ set2/16.py
@@ -1,11 +1,15 @@
 #Not optimized
-lower,upper=map(int,input().split())
+lower,upper=map(str,input().split())
+try:
+  lower=int(lower)
+  upper=int(upper)
   for num in range(lower,upper + 1):
     if num > 1:
-       for i in range(2,num):
-           if (num % i) == 0:
-               break
-       else:
-           print(num,end=" ")
-   
+      for i in range(2,num):
+        if (num % i) == 0:
+          break
+      else:
+        print(num,end=" ")
+except:
+  print("invalid")
    
