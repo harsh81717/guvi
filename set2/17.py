bd2f0aa9c76ebf85ab05448858bd24697cd372ea
harsh81717 <45117557+harsh81717@users.noreply.github.com>
2018-11-21 23:27:14 +0530

    Create 17.py

---
+++ set2/17.py
@@ -0,0 +1,12 @@
+n=int(input())
+length= len(str(n))
+sum = 0
+temp = n
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** length
+   temp //= 10
+if n == sum:
+   print("yes")
+else:
+   print("no")
